af1c61bb2ff70551650c282b54237e81378e78e0
Danila Verstov <VerstovDI@yandex.ru>
2020-09-06 13:52:16 +0300

    Fix task05 from part 5

--- Java/FirstProject/src/main/java/ru/mephi/java/part5/task05/Main.java
+++ Java/FirstProject/src/main/java/ru/mephi/java/part5/task05/Main.java
@@ -1,14 +1,17 @@
 package ru.mephi.java.part5.task05;
 
+import ru.mephi.java.part5.task01.Task01;
+
 import java.io.IOException;
 
 public class Main {
-    private static final String filenameFrom = "task01.txt";
-    private static final String filenameTo = "task05_output.txt";
+    private static final String basePath = Task01.getResourcesFilePath();
+    private static final String filenameFrom = basePath.concat("task01.txt");
+    private static final String filenameTo = basePath.concat("task05_output.txt");
 
     public static void main(String[] args) throws IOException {
         Task05 task05 = new Task05();
-        System.out.println(task05.method1(filenameFrom, filenameTo));
         System.out.println(task05.method2(filenameFrom, filenameTo));
+        System.out.println(task05.method1(filenameFrom, filenameTo));
     }
 }
--- Java/FirstProject/src/main/java/ru/mephi/java/part5/task05/Task05.java
+++ Java/FirstProject/src/main/java/ru/mephi/java/part5/task05/Task05.java
@@ -1,7 +1,5 @@
 package ru.mephi.java.part5.task05;
 
-import ru.mephi.java.part5.task01.Task01;
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -14,32 +12,34 @@ public class Task05 {
         try (Scanner in = new Scanner(new File(filenameFrom));
              PrintWriter out = new PrintWriter(filenameTo)) {
             while (in.hasNext()) {
-                resultString.append(in.next().toLowerCase());
+                resultString.append(in.next().toLowerCase()).append(" ");
             }
-            out.print(resultString);
+            out.print(resultString.toString().trim());
         }
-        return resultString.toString();
+        return resultString.toString().trim();
     }
 
     public String method2(String filenameFrom, String filenameTo) {
         StringBuilder resultString = new StringBuilder();
         Scanner in = null;
-        File file = null;
+        File file;
         PrintWriter printWriterOut = null;
         try {
             file = new File(filenameFrom);
             in = new Scanner(file);
             printWriterOut = new PrintWriter(filenameTo);
             while (in.hasNext()) {
-                resultString.append(in.next().toLowerCase());
+                resultString.append(in.next().toLowerCase()).append(" ");
             }
-            printWriterOut.print(resultString);
+            printWriterOut.print(resultString.toString().trim());
         } catch (NullPointerException npEx) {
             System.err.println(filenameFrom + " caused because of null pathname");
         } catch (FileNotFoundException fileNotFoundEx) {
             System.err.println("File " + filenameFrom + " wasn't found");
         } catch (SecurityException securityEx) {
             System.err.println("Do not have necessary access to file" + filenameTo);
+        } catch (IllegalStateException illegalStateEx) {
+            System.err.println("Scanner is closed, data can't be written");
         } finally {
             // First close last
             if (in != null) {
@@ -49,7 +49,6 @@ public class Task05 {
                 printWriterOut.close();
             }
         }
-        return resultString.toString();
+        return resultString.toString().trim();
     }
-
 }
