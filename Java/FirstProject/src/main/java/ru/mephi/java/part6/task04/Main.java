ce50d1905d9b115571f0d074da9a46afeb188e57
Danila Verstov <VerstovDI@yandex.ru>
2020-10-23 20:34:25 +0300

    Task 3 minor fixes + Task 4

--- Java/FirstProject/src/main/java/ru/mephi/java/part6/task04/Main.java
+++ Java/FirstProject/src/main/java/ru/mephi/java/part6/task04/Main.java
@@ -2,6 +2,23 @@ package ru.mephi.java.part6.task04;
 
 public class Main {
     public static void main(String[] args) {
-
+        Table<Integer, String> table = new Table<>();
+        table.add(1, "Aidarche");
+        table.add(2, "Danila");
+        table.add(3, "Kirill");
+        table.add(4, "Sasha");
+        table.add(5, "Vanya");
+        table.add(6, "Karpen");
+        table.add(7, "Pokek");
+        // TODO: task 3 & task 4 -> вынести тестирование в отдельный метод, убрать дубликаты
+        table.add(8, "Vasilina");
+        System.out.println(table);
+        System.out.println(table.get(1));
+        System.out.println(table.get(8));
+        //System.out.println(table.get(9));
+        table.set(7, "Kopek");
+        System.out.println(table.get(7));
+        table.remove(2);
+        System.out.println(table);
     }
 }
--- Java/FirstProject/src/main/java/ru/mephi/java/part6/task04/Table.java
+++ Java/FirstProject/src/main/java/ru/mephi/java/part6/task04/Table.java
@@ -1,19 +1,57 @@
-package ru.mephi.java.part6.task03;
+package ru.mephi.java.part6.task04;
+
+import ru.mephi.java.part6.task03.Entry;
 
 import java.util.ArrayList;
 import java.util.Objects;
 
 public class Table<K, V> {
-    private ArrayList<Entry<K, V>> entryList;
+    private ArrayList<Entry> entryList;
+
+    /* TODO: если вложенный (не статический внутренний), то не нужно - т.к.
+    объемлющий класс имеет доступ ко всем полям вложенного.
+    Если статический внутренний - то нужно, ведь имеем доступ только к статическим полям!
+     */
+
+    private class Entry {
+        private K key;
+        private V value;
+
+        private Entry(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public K getKey() {
+            return key;
+        }
+
+        public V getValue() {
+            return value;
+        }
+
+        @Override
+        public String toString() {
+            return "Entry{" +
+                    "key=" + key +
+                    ", value=" + value +
+                    '}';
+        }
+
+        public void setValue(V value) {
+            this.value = value;
+        }
+    }
 
     public Table() {
         entryList = new ArrayList<>();
     }
 
-    public void add(Entry<K, V> entry) {
-        entryList.add(entry);
+    public void add(K key, V value) {
+        entryList.add(new Entry(key, value));
     }
 
+    //Hashtable
     public V get(K key) {
         return Objects.requireNonNull(entryList.stream()
                 .filter(s -> s.getKey() == key)
@@ -44,7 +82,8 @@ public class Table<K, V> {
             }
         }
         return null;
-    }*/
+    }
+    */
 
     @Override
     public String toString() {
