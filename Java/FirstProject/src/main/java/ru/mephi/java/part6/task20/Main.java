94b6b74c0ad081975132ad352f807e0dc9f81c71
Danila Verstov <VerstovDI@yandex.ru>
2020-10-28 13:34:01 +0300

    Task 20 DONE

--- Java/FirstProject/src/main/java/ru/mephi/java/part6/task20/Main.java
+++ Java/FirstProject/src/main/java/ru/mephi/java/part6/task20/Main.java
@@ -1,11 +1,21 @@
 package ru.mephi.java.part6.task20;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(Arrays.toString(repeat(2, List.of(1, 2, 3, 4, 5))));
     }
 
-    /*@SafeVarargs public static final <T> T[] repeat(int n, T... objs) {
-
-    }*/
+    @SafeVarargs public static final <T> T[] repeat(int n, T... objs) {
+        List<T> resList = new ArrayList<>(n * objs.length);
+        T[] result = (T[]) Array.newInstance(objs.getClass(), n * objs.length);
+        for (int i = 0; i < n; i++) {
+            resList.addAll(Arrays.asList(objs));
+        }
+        return (T[]) resList.toArray();
+    }
 }
